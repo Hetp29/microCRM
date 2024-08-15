854c9adc1c1d88ef28494521f821a9ecd0625839
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-15 17:18:08 -0400

    Included API views and endpoints

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import RecordListCreate, RecordRetrieveUpdateDestroy
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/records/', RecordListCreate.as_view(), name='record-list-create'),
+    path('api/records/<int:pk>/', RecordRetrieveUpdateDestroy.as_view(), name='record-retrieve-update-destroy'),
 ]
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Record
+from .serializers import RecordSerializer
 
 
-def home(request):
-    return render(request, 'home.html', {})
-
+class RecordListCreate(generics.ListCreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    
+class RecordRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
