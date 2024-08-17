d11e1ae0cd94931d6eefafeeaaaae04ee0f22595
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-17 17:44:35 -0400

    Added Chakra UI custom theme with colors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f0e4ff",
+      100: "#cbb2ff",
+      200: "#a480ff",
+      300: "#7a4dff",
+      400: "#641bfe",
+      500: "#5a01e5",
+      600: "#5200b3",
+      700: "#430081",
+      800: "#2d004f",
+      900: "#14001f",
+    },
+  },
+  fonts: {
+    heading: `'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+});
 
 root.render(
   <ChakraProvider>
