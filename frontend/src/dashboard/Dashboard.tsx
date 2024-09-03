e0b05f0abc2fac9322485e8708f55a3bf8bfd380
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-03 14:22:58 -0400

    Added logout button

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Dashboard = () => {
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate('/');
+  }
   console.log('Dashboard component is rendering');
   return (
     <Box p={8} textAlign="center" bg="yellow">
@@ -11,6 +18,9 @@ const Dashboard = () => {
       <Text fontSize="lg" color="gray.600">
         Your dashboard is ready to manage your projects and clients efficiently.
       </Text>
+      <Button colorScheme='red' onClick={handleLogout}>
+        Log Out
+      </Button>
     </Box>
   );
 };
