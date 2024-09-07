a2acc11b12846b1f826969845b30e5899fdf764e
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-07 14:20:59 -0400

    Email config working in backend

--- backend/website/custom_email_backend.py
+++ backend/website/custom_email_backend.py
@@ -1,5 +1,9 @@
 from django.core.mail.backends.smtp import EmailBackend
 
+#SSL verification is process of checking validity of SSL certificate 
+#SSL (secure sockets layer) is security protocol that establishes encrypted link 
+#between web server and browser, ensuring data transferred between them remains 
+#private and secure.
 class CustomEmailBackend(EmailBackend):
     def _get_ssl_context(self):
         import ssl
@@ -7,5 +11,4 @@ class CustomEmailBackend(EmailBackend):
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
         return context
-    
     
