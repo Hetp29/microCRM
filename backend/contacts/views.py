9df7b5bb6664731cac8e9477e20029e39efe2b8c
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-22 10:56:21 -0400

    Set up views to handle API requests

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contact
+from .serializers import ContactSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [IsAuthenticated]
