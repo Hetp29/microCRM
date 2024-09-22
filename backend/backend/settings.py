1ab622bbd70ae4b5ec97b0a1d5e7819061ad74c3
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-22 10:53:18 -0400

    Defined contact model and made migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "website",
+    "contacts",
     "corsheaders",
     'django.contrib.sites',
     'allauth',
---
+++ backend/contacts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
