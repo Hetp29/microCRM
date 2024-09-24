0a503799c208c5fafa513cf85500b6d227cd6275
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-24 11:14:37 -0400

    Added sidebar on Contact management page

--- frontend/src/dashboard/contacts/ContactsPage.tsx
+++ frontend/src/dashboard/contacts/ContactsPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Flex, Box } from '@chakra-ui/react';
 import ContactsList from './ContactsList';
 import ContactForm from './ContactForm';
+import { Sidebar } from '../Sidebar'; // Import Sidebar
 import { Contact } from './types';
 
 const ContactsPage: React.FC = () => {
@@ -9,14 +10,19 @@ const ContactsPage: React.FC = () => {
   const [contacts, setContacts] = useState<Contact[]>([]);
 
   return (
-    <Flex h="100vh" p={4} bg="blue.50">
-      {/* Left side: Contacts List */}
+    <Flex h="100vh">
+      {/* Left side: Sidebar */}
+      <Box w="20%" bg="gray.50" borderRight="1px solid gray">
+        <Sidebar userTier="Professional" />
+      </Box>
+
+      {/* Center: Contacts List */}
       <Box w="30%" p={4} bg="gray.100" borderRight="1px solid gray">
         <ContactsList setSelectedContact={setSelectedContact} />
       </Box>
 
       {/* Right side: Contact Form */}
-      <Box flex="1" display="flex" justifyContent="flex-end" alignItems="flex-start" p={8} bg="white">
+      <Box flex="1" p={8} bg="white">
         <Box w="100%" maxW="600px" bg="yellow.100" border="1px solid gray" borderRadius="md" p={8}>
           <ContactForm editingContact={selectedContact} setEditingContact={setSelectedContact} setContacts={setContacts} />
         </Box>
