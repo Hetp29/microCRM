1cb335930a41b311b84cc7679cc9f5be7ec0eb1a
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-24 11:18:29 -0400

    UI changes

--- frontend/src/dashboard/contacts/ContactsPage.tsx
+++ frontend/src/dashboard/contacts/ContactsPage.tsx
@@ -11,19 +11,15 @@ const ContactsPage: React.FC = () => {
 
   return (
     <Flex h="100vh">
-      {/* Left side: Sidebar */}
-      <Box w="20%" bg="gray.50" borderRight="1px solid gray">
-        <Sidebar userTier="Professional" />
-      </Box>
+      
+      <Sidebar userTier="Professional" /> 
 
-      {/* Center: Contacts List */}
-      <Box w="30%" p={4} bg="gray.100" borderRight="1px solid gray">
+      <Box w="30%" p={4} >
         <ContactsList setSelectedContact={setSelectedContact} />
       </Box>
 
-      {/* Right side: Contact Form */}
       <Box flex="1" p={8} bg="white">
-        <Box w="100%" maxW="600px" bg="yellow.100" border="1px solid gray" borderRadius="md" p={8}>
+        <Box w="100%"  >
           <ContactForm editingContact={selectedContact} setEditingContact={setSelectedContact} setContacts={setContacts} />
         </Box>
       </Box>
